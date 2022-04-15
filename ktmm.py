f1ff0d9b3f1169991f18aa2582e4ef2233b53b76
bougenville <bougenville@Qomolangma.local>
2022-04-15 16:00:14 +0800

    print out the running time

--- ktmm.py
+++ ktmm.py
@@ -8,15 +8,18 @@ Created by:	Andrew Odendaal (https://github.com/ao/ktmm)
 from pynput.mouse import Controller
 import time
 
+def now():
+	t = time.localtime()
+	current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
+	print(current_time, '\n-------------------')
+
 if __name__ == '__main__':
 	mouse = Controller()
 	while True:
+		now()		
 		mouse.move(0.1, 0.1)
 		time.sleep(10)
-		print('Ho!')
 		mouse.move(-0.1, -0.1)
 		time.sleep(10)
-		print('Ha!')
-
 
 	# Oh damn, that was simple!
